cc44e41c7b33b4032598a580d51cc75b17044259
cdrivera3 <110323289+cdrivera3@users.noreply.github.com>
2024-07-29 15:43:00 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "723662244742807552"; // your league ID
+export const leagueID = "1121308465248542720"; // your league ID
 export const leagueName = "Taco League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
